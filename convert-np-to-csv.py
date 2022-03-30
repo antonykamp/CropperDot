6a3c4692e389fe372d9edd9618f8df01fbe46e52
Antony Kamp <tony.kamp@web.de>
2022-03-30 15:32:42 +0200

    convert npy to png and csv

---
+++ convert-np-to-csv.py
@@ -0,0 +1,14 @@
+import numpy
+import matplotlib.pyplot as plt
+import sys
+
+data = numpy.load(sys.argv[1], allow_pickle=True)
+i = 0
+name = sys.argv[1].split("/")[1].split(".")[0]
+print("loaded {}".format(name))
+for d in data:
+    img = plt.imshow(d, cmap='bwr')
+    plt.axis('off')
+    plt.savefig("./public/images/{}-{}.png".format(name, i), pad_inches=0, bbox_inches='tight')
+    numpy.savetxt("./files-csv/{}-{}.csv".format(name, i), d, delimiter=",")
+    i = i+1
